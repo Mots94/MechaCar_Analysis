02605e5cf75a27e735c0bfdde2e691769d7847e5
Mots94 <rmotykow@gmail.com>
2022-06-22 17:17:44 -0500

    deliverable 2 script complete

--- MechaCarChallenge_Script.R
+++ MechaCarChallenge_Script.R
@@ -10,9 +10,15 @@ summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AW
 
 sus_coils <- read.csv('Suspension_Coil.csv', header=T, stringsAsFactors=F)
 
-total_summary <- summarize(sus_coils$PSI, .groups = 'keep')
+total_summary <- sus_coils %>% summarize(Mean=mean(PSI), 
+                                         Median=median(PSI), 
+                                         Variance=var(PSI), 
+                                         SD=sd(PSI), 
+                                         .groups = 'keep')
 
-lot_summary <- sus_coils %>% groupby(Manufacturing_Lot) %>% summarize(sus_coils$PSI)
+lot_summary <- sus_coils %>% 
+  group_by(Manufacturing_Lot) %>% 
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
 
 # T-tests for suspension coils
 
